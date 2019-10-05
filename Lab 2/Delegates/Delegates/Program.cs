0c1d5ccbc664f75840951f3031b14078f2a2bedd
Francisco Matos <francisco_matos98@hotmail.com>
2019-10-05 20:22:19 +0100

    Delegates and Events exercises

---
+++ Lab 2/Delegates/Delegates/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Delegates
+{
+    delegate void MyDelegate(string s);
+
+    class Program
+    {
+        public static void Hello(string s)
+        {
+            Console.WriteLine("  Hello, {0}!", s);
+        }
+
+        public static void Goodbye(string s)
+        {
+            Console.WriteLine("  Goodbye, {0}!", s);
+        }
+
+        public static void DelegateTest(MyDelegate d)
+        {
+            Console.WriteLine("Invoking delegate inside method:");
+            d("Inside Method");
+        }
+
+        public static void Main()
+        {
+            MyDelegate a, b, c, d, e;
+
+            // Create the delegate object a that references
+            // the method Hello:
+            a = new MyDelegate(Hello);
+            // Create the delegate object b that references
+            // the method Goodbye:
+            b = new MyDelegate(Goodbye);
+            // The two delegates, a and b, are composed to form c:
+            c = a + b;
+            // Remove a from the composed delegate, leaving d,
+            // which calls only the method Goodbye:
+            d = c - a;
+            // Calling a null delegate
+            e = null;
+            // Attributing a new method to a delegate with methods in it
+            c = new MyDelegate(Hello);
+
+            Console.WriteLine("Invoking delegate a:");
+            a("A");
+            Console.WriteLine("Invoking delegate b:");
+            b("B");
+            Console.WriteLine("Invoking delegate c:");
+            c("C"); // old methods are overriden by the new ones
+            Console.WriteLine("Invoking delegate d:");
+            d("D");
+            // Console.WriteLine("Invoking delegate e:");
+            // e("E"); 
+            // Internal error
+
+            // testing delegate call inside method
+            DelegateTest(d);
+        }
+    }
+}
---
+++ Lab 2/Events/Events/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Events
+{
+    // esta  classe contem os argumentos do evento move do slider
+    public class MoveEventArgs : EventArgs
+    {
+        private int position;
+        public MoveEventArgs(int position)
+        {
+            this.position = position;
+        }
+        public int Position { get { return position; } }
+
+    }
+
+
+    class Slider
+    {
+        private int position;
+
+        // declaração do delegate
+        public delegate void Move(object source, MoveEventArgs e);
+        // declaração do evento
+        public event Move moveEvent;
+
+        public int Position
+        {
+            get
+            {
+                return position;
+            }
+            // e' este bloco que e' executado quando se move o slider
+            set
+            {
+                // call delegate method before setting position
+                // validate event is an object of type Move delegate, so it would be null if no class is subscribed
+                // to the event so that's why it is necessary to check if it's null before calling a delegate
+                if (moveEvent != null)
+                    moveEvent(this, new MoveEventArgs(value));
+
+                position = value;
+            }
+        }
+    }
+
+
+    class Form
+    {
+        static void Main()
+        {
+            Slider slider = new Slider();
+
+            // subscribe to moveEvent
+            slider.moveEvent += slider_Move;
+
+            // estas sao as duas alteracoes simuladas no slider
+            slider.Position = 20;
+            slider.Position = 60;
+        }
+
+        // este é o método que deve ser chamado quando o slider e' movido
+        static void slider_Move(object source, MoveEventArgs e)
+        {
+            if (e.Position > 50)
+                Console.WriteLine("Invalid Position: {0}", e.Position);
+            else
+                Console.WriteLine("Valid Position: {0}", e.Position);
+        }
+    }
+
+}
