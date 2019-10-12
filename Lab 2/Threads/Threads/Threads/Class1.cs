d29d32459175366d95505d14be7b0ccbcd8f8f91
danielaLopes <danielalopes_97@hotmail.com>
2019-10-12 16:56:41 +0100

    fixed Threads exercise in Lab2

---
+++ Lab 2/Threads/Threads/Threads/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    /**
+     * Circular buffer to store requests for threads in the ThreadPool to execute.
+     * Has a limited size for writes and a read is always done to the first 
+     * element of the buffer. When a read is executed, the element is removed from 
+     * the list and the more recent requests go to the front of the queue.
+     * First in first out (FIFO).
+     */
+    class RequestsCircularBuffer
+    {
+        private Queue<ThrWork> _requestsQueue = new Queue<ThrWork>();
+        private readonly int _maxSize;
+
+        public RequestsCircularBuffer(int maxSize)
+        {
+            _maxSize = maxSize;
+        }
+
+        public int getRequestsCount()
+        {
+            return _requestsQueue.Count;
+        }
+
+        /** Equivalent to a dequeue */
+        public ThrWork ReadRequest()
+        {
+            lock (_requestsQueue)
+            {
+                // No requests to read, wait until a new request goes into the queue
+                while (_requestsQueue.Count == 0)
+                {
+                    Console.WriteLine("Read - Thread {0} is sleeping", Thread.CurrentThread.ManagedThreadId);
+                    Monitor.Wait(_requestsQueue);
+                }
+                Console.WriteLine("Read - Thread {0} is reading", Thread.CurrentThread.ManagedThreadId);
+                ThrWork request = _requestsQueue.Dequeue();
+                if (_requestsQueue.Count == _maxSize - 1)
+                {
+                    // wake up some blocked thread to come write a new request
+                    Monitor.PulseAll(_requestsQueue); // needs to have pulseAll!
+                }
+
+                return request;
+            }
+        }
+
+        /** Equivalent to an enqueue */
+        public void WriteRequest(ThrWork request)
+        {
+            lock (_requestsQueue)
+            {
+                // No space for more requests, wait until a request is processed 
+                while (_requestsQueue.Count >= _maxSize)
+                {
+                    Console.WriteLine("Write - Thread {0} is sleeping", Thread.CurrentThread.ManagedThreadId);
+                    Monitor.Wait(_requestsQueue);
+                }
+                Console.WriteLine("Write - Thread {0} is writing", Thread.CurrentThread.ManagedThreadId);
+                _requestsQueue.Enqueue(request);
+                if (_requestsQueue.Count == 1)
+                {
+                    // wake up some blocked thread to come process request
+                    Monitor.PulseAll(_requestsQueue); // needs to have pulseAll!
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string queue = "";
+            for(int i = 0; i < _requestsQueue.Count; i++) {
+                queue += "Request number : " + i + "\r\n";
+            }
+
+            return queue;
+        }
+    }
+}
--- Lab 2/Threads/Threads/Threads/Program.cs
+++ Lab 2/Threads/Threads/Threads/Program.cs
@@ -7,27 +7,68 @@ namespace Threads
 
     class ThrPool
     {
-        //private readonly int _bufSize;
-        //private int _nRequests;
+        /** Threads */
+        private Thread[] threadPool;
+        /** Requests */
+        private RequestsCircularBuffer _requestsQueue;
+        /** Decide when the thread pool stops running */
+        private bool _running;
 
         public ThrPool(int thrNum, int bufSize)
         {
-            // TODO
-            ThreadPool.SetMaxThreads(5, 5);
-            ThreadPool.SetMinThreads(1, 1);
+            Running = true;
 
-            //_bufSize = bufSize;
-           // _nRequests = 0;
+            /** Creates the request queue with limited size */
+            _requestsQueue = new RequestsCircularBuffer(bufSize);
+
+            /** Creates thread pool */
+            threadPool = new Thread[thrNum];
+            for (int i = 0; i < thrNum; i++)
+            {
+                threadPool[i] = new Thread(ProcessRequests);
+                threadPool[i].Start();
+            }
+
+            /*foreach(Thread thread in threadPool)
+            {
+                Console.WriteLine(thread.ManagedThreadId);
+            }*/
+        }
+
+        public bool Running
+        {
+            set
+            {
+                _running = value;
+            }
+            get
+            {
+                return _running;
+            }
         }
 
         /*
-         * Method for asynchronous execution of the delegate ThrWork
+         * Method for asynchronous execution of the delegate ThrWork.
+         * Writes requests into the request queue.
          */
         public void AssyncInvoke(ThrWork action)
         {
-            // TODO
-            // check if we need to limit the number of requests ?????
-            ThreadPool.QueueUserWorkItem(delegate { action(); });
+            _requestsQueue.WriteRequest(action);
+        }
+
+        /** 
+         * Method to be execute by every thread in the thread pool.
+         * Checks if there are requests to be processed in the request queue.
+         * If there are requests, one of the threads processes it, otherwise it waits until
+         * a request is inserted in the request queue.
+         */
+        public void ProcessRequests()
+        {
+            while (_running)
+            {
+                _requestsQueue.ReadRequest().Invoke();//.BeginInvoke(null, null, null);
+            }
+            Console.WriteLine("Thread finished");
         }
     }
 
@@ -67,6 +108,14 @@ namespace Threads
         }
     }
 
+    class EndProgram
+    {
+        public void End()
+        {
+            Console.WriteLine("Program finished executing all requests");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -76,17 +125,27 @@ namespace Threads
              * 5 threads
              * 10 requests (buffer size)
              */
-            ThrPool tpool = new ThrPool(5, 10);
+            const int nThreads = 5;
+            const int nRequests = 10;
+
+            ThrPool tpool = new ThrPool(nThreads, nRequests);
             /** Delegate */
-            ThrWork work = null; // why this delegate ????
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < (nRequests / 2) ; i++)
             {
                 A a = new A(i);
                 tpool.AssyncInvoke(new ThrWork(a.DoWorkA));
                 B b = new B(i);
                 tpool.AssyncInvoke(new ThrWork(b.DoWorkB));
             }
-            Console.ReadLine(); // whythis realine ??????
+            Console.ReadLine();
+
+            /** stops threads from reading requests */
+            tpool.Running = false;
+            EndProgram end = new EndProgram();
+            for(int thread = 0; thread < nThreads ; thread++)
+            {
+                tpool.AssyncInvoke(new ThrWork(end.End));
+            }
         }
     }
 }
